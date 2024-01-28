364ddf9bcd4c561eb1f5f449c0a43e4c90672ac6
TkachAn <4ndrey.tkachenko@gmail.com>
2024-01-28 11:15:20 +0200

    create header

--- polus/src/components/header/header.jsx
+++ polus/src/components/header/header.jsx
@@ -1,7 +1,44 @@
 import s from "./header.module.css";
 
-export const Header = ()=>{
-return(
-	<h1 className={s.h1}>Header</h1>
-)
+export const Header = () => {
+  return (
+    <header className={s.wrapper}>
+      <div className="container">
+        <div className={s.flex}>
+          <section className={s.section}>
+            <div className={s.logo}>polus yug</div>
+          </section>
+          <section className={s.section}>
+            <div className={s.phone}>
+              tel.: <span>+380(512)711690</span>
+            </div>
+          </section>
+          <section className={s.section}>
+						<div className={s.burger}>
+							<span></span>
+						</div>
+            <nav className={s.navigation}>
+              <ul className={s.flexNav}>
+                <li className={s.item}>
+                  <a href="/">home</a>
+                </li>
+                <li>
+                  <a href="/products">products</a>
+                </li>
+                <li>
+                  <a href="/downloads">hdownloads</a>
+                </li>
+                <li>
+                  <a href="/about">abuot us</a>
+                </li>
+                <li>
+                  <a href="/contacts">contacts</a>
+                </li>
+              </ul>
+            </nav>
+          </section>
+        </div>
+      </div>
+    </header>
+  );
 };
--- polus/src/pages/homePage.jsx
+++ polus/src/pages/homePage.jsx
@@ -1,5 +1,17 @@
 import s from "./page.module.css";
 
-export const Home = ()=>{
-	return(<h1 className={s.h1}>Home page</h1>)
-}
+export const Home = () => {
+  return (
+    <>
+			<article>
+      <h1 className={s.h1}>Home page</h1>
+<section>
+	<h2>h2</h2>
+	<p>
+		k.ugflgoiy;'u'pup
+	</p>
+</section>
+			</article>
+    </>
+  );
+};
