3d59b7689dc14b0526bf63280244dcc5eb50322c
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-01 09:27:33 +0200

    header str plus navigation

--- polus/src/components/header/header.jsx
+++ polus/src/components/header/header.jsx
@@ -1,37 +1,36 @@
+import React, {useState} from "react";
 import s from "./header.module.css";
 
 export const Header = () => {
+	const [isOpen, setOpen] = useState(false);
   return (
     <header className={s.wrapper}>
       <div className="container">
         <div className={s.flex}>
-          <section className={s.section}>
+          <section className={s.section_logo}>
             <div className={s.logo}>polus yug</div>
           </section>
           <section className={s.section}>
-            <div className={s.phone}>
-              tel.: <span>+380(512)711690</span>
-            </div>
-          </section>
-          <section className={s.section}>
-						<div className={s.burger}>
-							<span></span>
-						</div>
+						<button className={s.burger} onClick={() => setOpen(!isOpen)}>
+							<div className={s.bar2}></div>
+							<div className={s.bar1}></div>
+							<div className={s.bar3}></div>
+						</button>
             <nav className={s.navigation}>
-              <ul className={s.flexNav}>
-                <li className={s.item}>
+              <ul className={isOpen ? s.burger_nav : s.flexNav}>
+                <li className={s.nav_item}>
                   <a href="/">home</a>
                 </li>
-                <li>
+                <li className={s.nav_item}>
                   <a href="/products">products</a>
                 </li>
-                <li>
+                <li className={s.nav_item}>
                   <a href="/downloads">hdownloads</a>
                 </li>
-                <li>
+                <li className={s.nav_item}>
                   <a href="/about">abuot us</a>
                 </li>
-                <li>
+                <li className={s.nav_item}>
                   <a href="/contacts">contacts</a>
                 </li>
               </ul>
