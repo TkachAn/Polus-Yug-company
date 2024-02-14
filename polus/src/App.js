f7af894644dd317562172901632b5aee25d9b540
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-14 10:29:53 +0200

    Update App.js

--- polus/src/App.js
+++ polus/src/App.js
@@ -1,19 +1,19 @@
 import {Route, Routes} from "react-router-dom";
 import {Home} from "./pages/homePage.jsx";
-import { NotFound } from "./pages/notFoundPage.jsx";
-import { ShareLayout } from "./components/layOut/sharedLayout.jsx";
-import { Contacts } from "./pages/contactsPage.jsx";
-import { About } from "./pages/aboutPage.jsx";
-import './App.css';
+import {NotFound} from "./pages/notFoundPage.jsx";
+import {ShareLayout} from "./components/layOut/sharedLayout.jsx";
+import {Contacts} from "./pages/contactsPage.jsx";
+import {About} from "./pages/aboutPage.jsx";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ShareLayout/>}>
+      <Route path="/" element={<ShareLayout />}>
         <Route index element={<Home />} />
-				{/* <Route path="/cold" element={<Cold />} /> */}
-				{/* <Route path="/leds" element={<Leds />} /> */}
-				<Route path="/contacts" element={<Contacts />} />
+        {/* <Route path="/cold" element={<Cold />} /> */}
+        {/* <Route path="/leds" element={<Leds />} /> */}
+        <Route path="/contacts" element={<Contacts />} />
         <Route path="/about" element={<About />}>
           {/* <Route path="mission" element={<Mission />} /> */}
           {/* <Route path="team" element={<Team />} /> */}
@@ -21,10 +21,9 @@ function App() {
         </Route>
         {/* <Route path="/products" element={<Products />} /> */}
         {/* <Route path="/products/:id" element={<ProductDetails />} /> */}
-      <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-				
   );
 }
 
